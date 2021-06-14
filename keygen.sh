5b37b9f5e4ead7e9200f11490893625718b066ee
Atanas Bachvaroff <bachvaroff@gmail.com>
2021-06-14 08:39:51 +0300

    xtea

--- keygen.sh
+++ keygen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for i in `seq 0 16`; do
-	dd if=/dev/random bs=1 count=16 2>/dev/null | hd | sed -e 's/^........//g' -e 's/|.*$//g' -e '/^$/d' -e 's/ //g'
+	dd if=/dev/random bs=1 count=16 2>/dev/null | hd | sed -e 's/^........//g' -e 's/|.*$//g' -e '/^$/d' -e 's/ //g' -e 's/......../& /g'
 done
 
