be66496892eeb3229173b339121494d816d1615a
Atanas Bachvaroff <bachvaroff@gmail.com>
2021-06-14 10:35:59 +0300

    xtea

--- keygen.sh
+++ keygen.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-for i in `seq 0 16`; do
-	dd if=/dev/random bs=1 count=16 2>/dev/null | hd | sed -e 's/^........//g' -e 's/|.*$//g' -e '/^$/d' -e 's/ //g' -e 's/......../& /g'
-done
-
+(
+	for i in `seq 0 15`; do
+		dd if=/dev/random bs=1 count=16 2>/dev/null | hd | sed \
+			-e 's/^........//g' \
+			-e 's/|.*$//g' \
+			-e '/^$/d' \
+			-e 's/ //g' \
+			-e 's/......../& /g'
+	done
+) | awk 'BEGIN { n = 0; } { printf("%d ", n); print; n++; }'
