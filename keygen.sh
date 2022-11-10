51d8fe8e1845289f5156adb852febab54614d982
Atanas Bachvaroff <bachvaroff@gmail.com>
2022-11-10 13:53:46 +0200

    10112022

--- keygen.sh
+++ keygen.sh
@@ -2,11 +2,7 @@
 
 (
 	for i in `seq 0 15`; do
-		dd if=/dev/random bs=1 count=16 2>/dev/null | hd | sed \
-			-e 's/^........//g' \
-			-e 's/|.*$//g' \
-			-e '/^$/d' \
-			-e 's/ //g' \
-			-e 's/......../& /g'
+		dd if=/dev/random bs=1 count=16 2>/dev/null | \
+			hd | sed -e 's/^[0-9a-fA-F]*//g;s/|.*$//g;s/ //g;s/......../& /g;/^$/d'
 	done
 ) | awk 'BEGIN { n = 0; } { printf("%d ", n); print; n++; }'
